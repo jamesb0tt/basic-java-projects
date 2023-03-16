b27a1aea79e1fd87df5bde7369082b3b073d607e
jamesb0tt <126026261+jamesb0tt@users.noreply.github.com>
2023-03-16 11:58:40 -0400

    Update KeyboardInputs.java

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,7 +3,7 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import gamebuildingvideo.gamepanel;
+import movingsquare.gamepanel;
 
 public class KeyboardInputs implements KeyListener{
 
